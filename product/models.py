4513050f3a147884001fbc6a66fcfd7104d8874b
huyuty <pavel141621@mail.ru>
2022-12-02 11:49:16 +0900

    fdsfsdf

--- product/models.py
+++ product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
-    image = models.ImageField(upload_to='static/img/')
+    image = models.ImageField(upload_to='img')
 
     def __str__(self):
         return self.name
--- product/views.py
+++ product/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from product.models import *
 
-# Create your views here.
+from django.views.generic import TemplateView, ListView, DetailView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class CategoriesView(ListView):
+    model = Product
+    template_name = 'categories.html'
+
+
+class CheckView(TemplateView):
+    template_name = 'check-out.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product-page.html'
+
+
+class CartView(TemplateView):
+    template_name = 'shopping-cart.html'
+
